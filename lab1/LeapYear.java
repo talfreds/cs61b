f4104dd093e49a77302a913e1846eeaa7bcfec29
sissi <sissi@byrsp.com>
2018-05-03 16:31:06 +0800

    construct isLeapYear() method

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,6 +1,7 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Sissi
  */
+
 public class LeapYear {
 
     /** Calls isLeapYear to print correct statement.
@@ -12,6 +13,15 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if (year % 400 == 0) {
+            return true;
+        } else if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
+        return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
